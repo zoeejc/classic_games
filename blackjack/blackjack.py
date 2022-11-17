97b72be0f0a34724d9010b113d242ced4c781502
Zoe Collins <zoe.collins2@mail.dcu.ie>
2022-11-17 14:14:04 +0000

    Added the dealer and updated rules.txt.

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -9,10 +9,12 @@ import os
 # do different things based on input
 # twist, stick, split
 # A is default 11, if you go over 21 it changes to 1
+# dealer twists until they have 17 or above
+
 
 os.system('clear')
 
-cards = {"1": 1,
+cards = {
 		 "2": 2,
 		 "3": 3,
 		 "4": 4,
@@ -49,11 +51,12 @@ if start == "play":
 	mycard2 = r.choice(list(cards.keys()))
 	print(mycard1, mycard2)
 
+	total = cards[mycard1] + cards[mycard2]
+
+	print(f">>> Your total is {total}")
+
 	my_cards.append(mycard1)
 	my_cards.append(mycard2)
-
-	total = cards[mycard1] + cards[mycard2]
-	print(f">>> Your total is {total}")
 
 	print(">>> What would you like to do: twist, split or stick?")
 
@@ -84,6 +87,8 @@ if start == "play":
 				print(">>> You got a 5 card trick!! Congrats!!")
 				break
 
+			print(">>> What would you like to do: twist or stick?")
+
 			prompt = input()
 
 		elif prompt == "split":
@@ -99,6 +104,7 @@ if start == "play":
 					total -= 10
 					my_cards.remove("A")
 					print(f">>> You went bust, but you had an ace!! Total is now {total}!")
+					print(">>> What would you like to do: twist, or stick?")
 
 				elif total > 21:
 					print(">>> Oh no!! You went bust!!!")
@@ -115,7 +121,59 @@ if start == "play":
 
 			prompt = input()
 
-#	if prompt == "stick":
-#		dcard1 = r.choice(list(cards.keys()))
-#		dcard2 = r.choice(list(cards.keys()))
-#		print(f"The dealer's cards are: {dcard1} {dcard2}")
+if prompt == "stick":
+	d_total = 0
+	dcard1 = r.choice(list(cards.keys()))
+	dcard2 = r.choice(list(cards.keys()))
+	print(f">>> The dealer's cards are: {dcard1} {dcard2}")
+
+	d_total += cards[dcard1]
+	d_total += cards[dcard2]
+
+	dealer_cards.append(dcard1)
+	dealer_cards.append(dcard2)
+
+	if d_total >= 17:
+		print(">>> Dealer sticks!")
+
+	else:
+		dnext = r.choice(list(cards.keys()))
+		dealer_cards.append(dnext)
+		d_total += cards[dnext]
+		print(f">>> The dealer got {dnext}, their total is now {d_total}")
+		if d_total > 21 and ("A" in dealer_cards):
+				d_total -= 10
+				dealer_cards.remove("A")
+				print(f">>> The dealer went bust, but they had an ace!! Total is now {d_total}!")
+				if d_total < 17:
+					dnext = r.choice(list(cards.keys()))
+					dealer_cards.append(dnext)
+					d_total += cards[next]
+					print(f">>> The dealer got {dnext}, their total is now {d_total}")
+				elif d_total >= 17:
+					if d_total < total:
+						print(f">>> You beat the dealer with a total of {total}!")
+				elif d_total > total:
+						print(f">>> The dealer beat you with a total of {d_total}!")
+		if d_total < 17:
+				dnext = r.choice(list(cards.keys()))
+				dealer_cards.append(dnext)
+				d_total += cards[dnext]
+				print(f">>> The dealer got {dnext}, their total is now {d_total}")
+				if d_total >= 17:
+					if d_total < total:
+						print(f">>> You beat the dealer with a total of {total}!")
+				elif d_total > total:
+					print(f">>> The dealer beat you with a total of {d_total}!")
+
+		if d_total <= 21 and (len(dealer_cards) == 5):
+			print(">>> The dealer got a 5 card trick. They win!")
+
+
+	if d_total < total:
+			print(f">>> You beat the dealer with a total of {total}!")
+	elif d_total > total:
+		print(f">>> The dealer beat you with a total of {d_total}!")
+	else:
+		print(">>> Tie!")
+
